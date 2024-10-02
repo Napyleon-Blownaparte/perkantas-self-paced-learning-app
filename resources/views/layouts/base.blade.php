25acf2baec79f6a5e4d65a5df1d6ff781702ad67
Richard Kamitono <richardkamitono@gmail.com>
2024-10-03 01:10:11 +0700

    Landing Page UI Done

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PERKANTAS JATIM</title>
+        @vite('resources/css/app.css')
+        <link href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" rel="stylesheet"/>
+        <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    </head>
+    <body>
+        @yield('content')
+        
+    </body>
+</html>
+
+@yield('script')
