608d731cfcbb3602e5df807552f0ef1894b39b65
Zephylest <kiko.prayogo@gmail.com>
2024-12-15 16:11:19 +0700

    fix view carousel

--- resources/views/components/carousel-course.blade.php
+++ resources/views/components/carousel-course.blade.php
@@ -5,14 +5,16 @@
     <div class="flex space-x-4 overflow-x-auto col-span-2">
         @foreach ($courses as $course)
             <div class="min-w-96 max-w-96 bg-gray-500 p-4 rounded shadow-md">
-                <img src="{{ asset('storage/'.$course->thumbnail_image) }}" alt="Course Image" class="w-full rounded">
+                <img src="{{ asset('storage/'.$course->thumbnail_image) }}" alt="Course Image" class="object-cover w-[360px] h-[380px]">
                 <h3 class="mt-4 text-white text-2xl font-bold max-w-md">{{ $course->title }}</h3>
                 <p class="mt-2 text-white text-lg font-semibold">
                     Est. {{ $course->estimated_time }} Hour(s)</p>
                 <div class= "grid grid-cols-2 gap-6">
-                    <button href="{{ route('learner.courses.show', $course->id) }}"
-                        class="mt-4 bg-gray-500 text-white px-5 py-3 border-2 border-white">Learn
-                        More</button>
+                    <a href="{{ route('learner.courses.show', $course->id) }}">
+                        <button
+                            class="mt-4 bg-gray-500 text-white px-5 py-3 border-2 border-white">Learn
+                            More</button>
+                    </a>
                     <button class="mt-4 bg-blue-800 text-white px-5 py-3">Enroll</button>
                 </div>
             </div>
